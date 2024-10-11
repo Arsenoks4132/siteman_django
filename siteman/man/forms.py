6e1dd233333d514b979eaf12ff1b7544bb6f1c98
Arsenoks4132 <Danov2005@yandex.ru>
2024-10-11 22:21:29 +0300

    added validators to simple form

--- siteman/man/forms.py
+++ siteman/man/forms.py
@@ -1,14 +1,43 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+
 from .models import Category, Wife
+
+
+class RussianValidator:
+    ALLOWED_CHARS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789- '
+    code = 'Russian'
+
+    def __init__(self, message=None):
+        self.message = message if message else 'Должны присутствовать только русские символы, цифры, дефис и пробел.'
+
+    def __call__(self, value, *args, **kwargs):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
 
 
 class AddPostForm(forms.Form):
     title = forms.CharField(
+        min_length=5,
         max_length=255,
         label='Заголовок',
-        widget=forms.TextInput(attrs={'class': 'form-input'})
+        widget=forms.TextInput(attrs={'class': 'form-input'}),
+        error_messages={
+            'min_length': 'Слишком короткий заголовок',
+            'required': 'Заголовок обязателен'
+        },
+
     )
-    slug = forms.SlugField(max_length=255, label='URL')
+    slug = forms.SlugField(
+        min_length=5,
+        max_length=255,
+        label='URL',
+        validators=[
+            MinLengthValidator(5, message="Минимум 5 символов"),
+            MaxLengthValidator(100)
+        ]
+    )
     content = forms.CharField(
         widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}),
         required=False, label='Контент'
@@ -25,3 +54,9 @@ class AddPostForm(forms.Form):
         empty_label='Не женат',
         label='Супруга'
     )
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        ALLOWED_CHARS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789- '
+        if not (set(title) <= set(ALLOWED_CHARS)):
+            raise ValidationError('Должны присутствовать только русские символы, цифры, дефис и пробел.')
