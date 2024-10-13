a17e1e4746fde7e616e9e3fa17cf1398a42aa9a1
Arsenoks4132 <Danov2005@yandex.ru>
2024-10-13 18:27:27 +0300

    added registration

--- siteman/users/views.py
+++ siteman/users/views.py
@@ -4,8 +4,9 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -14,21 +15,20 @@ class LoginUser(LoginView):
     extra_context = {'title': 'Авторизация'}
 
 
-# def login_user(request: WSGIRequest):
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('users:login')
+
+# def register(request: WSGIRequest):
 #     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
+#         form = RegisterUserForm(request.POST)
 #         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'], password=cd['password'])
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
+#             user = form.save(commit=False)
+#             user.set_password(form.cleaned_data['password'])
+#             user.save()
+#             return render(request, 'users/register_done.html')
 #     else:
-#         form = LoginUserForm()
-#
-#     return render(request, 'users/login.html', context={'form': form})
-
-
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('users:login'))
+#         form = RegisterUserForm()
+#     return render(request, 'users/register.html', {'form': form})
