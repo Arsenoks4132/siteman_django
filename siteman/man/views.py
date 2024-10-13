0c7238fa6d159dcf000451897ed3748aecc79374
Arsenoks4132 <Danov2005@yandex.ru>
2024-10-13 13:41:04 +0300

    removed unneeded comments added pagination example

--- siteman/man/views.py
+++ siteman/man/views.py
@@ -1,4 +1,5 @@
 from django.core.handlers.wsgi import WSGIRequest
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect, reverse, get_object_or_404, get_list_or_404
 from django.urls import reverse_lazy
@@ -8,24 +9,6 @@ from django.views.generic import TemplateView, ListView, DetailView, FormView, C
 from .forms import AddPostForm, UploadFileForm
 from .models import Man, Category, TagPost, UploadFiles
 from .utils import DataMixin
-
-menu = [
-    {'title': 'О сайте', 'url_name': 'about'},
-    {'title': 'Добавить страницу', 'url_name': 'add_page'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'log_in'}
-]
-
-
-# def index(request):
-#     posts = Man.published.all().select_related('cat')
-#     data = {
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': 0
-#     }
-#     return render(request, 'man/index.html', context=data)
 
 
 class ManHome(DataMixin, ListView):
@@ -39,37 +22,17 @@ class ManHome(DataMixin, ListView):
     def get_queryset(self):
         return Man.published.all().select_related('cat')
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['title'] = 'Главная страница'
-    #     context['menu'] = menu
-    #     context['posts'] = Man.published.all().select_related('cat')
-    #     context['cat_selected'] = int(self.request.GET.get('cat_id', 0))
-    #     return context
-
-
-# def handle_uploaded_file(f):
-
-
-#     with open(f"uploads/{f.name}", "wb+") as destination:
-#         for chunk in f.chunks():
-#             destination.write(chunk)
-
 
 def about(request: WSGIRequest):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            # handle_uploaded_file(form.cleaned_data['file'])
-            fp = UploadFiles(file=form.cleaned_data['file'])
-            fp.save()
-    else:
-        form = UploadFileForm()
+    contact_list = Man.published.all()
+    paginator = Paginator(contact_list, 3)
+
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
 
     data = {
         'title': 'О нашем сайте',
-        'menu': menu,
-        'form': form,
+        'page_obj': page_obj,
     }
     return render(request, template_name='man/about.html', context=data)
 
@@ -97,17 +60,6 @@ def page_not_found(request, exception):
     return HttpResponseNotFound(f"<h3>Мы пока ничего не нашли, пока!!!</h3>")
 
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Man, slug=post_slug)
-#     data = {
-#         'title': post.title,
-#         'menu': menu,
-#         'post': post,
-#         'cat_selected': None,
-#     }
-#     return render(request, template_name='man/post.html', context=data)
-
-
 class ShowPost(DataMixin, DetailView):
     model = Man
     template_name = 'man/post.html'
@@ -122,75 +74,18 @@ class ShowPost(DataMixin, DetailView):
         return get_object_or_404(Man.published, slug=self.kwargs[self.slug_url_kwarg])
 
 
-# def add_page(request: WSGIRequest):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # print(form.cleaned_data)
-#             # try:
-#             #     Man.objects.create(**form.cleaned_data)
-#             #     uri = reverse('home')
-#             #     return redirect(uri)
-#             # except:
-#             #     form.add_error(None, 'Ошибка добавления поста')
-#             form.save()
-#             uri = reverse('home')
-#             return redirect(uri)
-#     else:
-#         form = AddPostForm()
-#
-#     data = {
-#         'title': "Добавление статьи",
-#         'menu': menu,
-#         'form': form
-#     }
-#     return render(request, template_name='man/add_page.html', context=data)
-
-class AddPage(CreateView):
+class AddPage(DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'man/add_page.html'
-    # success_url = reverse_lazy('home')
-
-    extra_context = {
-        'title': "Добавление статьи",
-        'menu': menu,
-    }
+    title_page = 'Добавление статьи'
 
 
-class UpdatePage(UpdateView):
+class UpdatePage(DataMixin, UpdateView):
     model = Man
     fields = ['title', 'content', 'photo', 'is_published', 'cat']
     template_name = 'man/add_page.html'
     success_url = reverse_lazy('home')
-
-    extra_context = {
-        'title': "Редактирование статьи",
-        'menu': menu,
-    }
-
-
-# class AddPage(View):
-#     def get(self, request: WSGIRequest):
-#         form = AddPostForm()
-#         data = {
-#             'title': "Добавление статьи",
-#             'menu': menu,
-#             'form': form
-#         }
-#         return render(request, template_name='man/add_page.html', context=data)
-#
-#     def post(self, request: WSGIRequest):
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             uri = reverse('home')
-#             return redirect(uri)
-#         data = {
-#             'title': "Добавление статьи",
-#             'menu': menu,
-#             'form': form
-#         }
-#         return render(request, template_name='man/add_page.html', context=data)
+    title_page = 'Редактирование статьи'
 
 
 def contact(request):
@@ -201,19 +96,7 @@ def log_in(request):
     return HttpResponse('БД пока нету, терпите')
 
 
-# def show_category(request, cat_slug):
-#     category = get_object_or_404(Category, slug=cat_slug)
-#     posts = Man.published.filter(cat=category).select_related('cat')
-#     data = {'title': f'Рубрика: {category.name}',
-#             'menu': menu,
-#             'posts': posts,
-#             'cat_selected': category.pk,
-#             }
-#
-#     return render(request, 'man/index.html', context=data)
-
-
-class ManCategory(ListView):
+class ManCategory(DataMixin, ListView):
     template_name = 'man/index.html'
     context_object_name = 'posts'
     allow_empty = False
@@ -224,27 +107,15 @@ class ManCategory(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cat = context['posts'][0].cat
-        context['title'] = f'Категория - {cat.name}'
-        context['menu'] = menu
-        context['cat_selected'] = cat.pk
-        return context
+
+        return self.get_mixin_context(
+            context,
+            title=f'Категория - {cat.name}',
+            cat_selected=cat.pk,
+        )
 
 
-# def show_tag_post_list(request, tag_slug):
-#     tag = get_object_or_404(TagPost, slug=tag_slug)
-#     posts = Man.published.filter(tags=tag)
-#
-#     data = {
-#         'title': f'Тeг: {tag.tag}',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': None,
-#     }
-#
-#     return render(request, 'man/index.html', data)
-
-
-class ManTag(ListView):
+class ManTag(DataMixin, ListView):
     template_name = 'man/index.html'
     context_object_name = 'posts'
     allow_empty = False
@@ -255,7 +126,7 @@ class ManTag(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         tag = context['posts'][0].tags.get(slug=self.kwargs['tag_slug'])
-        context['title'] = f'Тег - {tag.tag}'
-        context['menu'] = menu
-        context['cat_selected'] = None
-        return context
+        return self.get_mixin_context(
+            context,
+            title=f'Тег - {tag.tag}'
+        )
